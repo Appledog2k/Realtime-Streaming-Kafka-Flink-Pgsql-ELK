bc7731959816bab826f05efe8be25d9c949a9d32
Nguyễn Quang Hùng <150774868+hungnguyen-hust@users.noreply.github.com>
2024-03-12 13:57:04 +0700

    done build env 1

---
+++ Flink-Realtime/Flink-Realtime/src/main/java/Dto/Transaction.java
@@ -0,0 +1,17 @@
+package Dto;
+
+public class Transaction {
+    
+    private String transactionId;
+    private String productId;
+    private String productName;
+    private String productCategory;
+    private double productPrice;
+    private int productQuantity;
+    private String productBrand;
+    private double totalAmount;
+    private String currency;
+    private String customnerId;
+    private Timestamp transactionDate;
+    private String paymentMethod;
+}
